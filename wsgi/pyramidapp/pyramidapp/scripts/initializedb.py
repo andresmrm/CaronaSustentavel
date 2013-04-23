bd4aaa5c4c5333a83b5ebceb93b706a4c3d5fa02
andresmrm <andresmrm@gmail.com>
2013-04-23 11:44:44 -0300

    de

---
+++ wsgi/pyramidapp/pyramidapp/scripts/initializedb.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import transaction
+
+from sqlalchemy import engine_from_config
+
+from pyramid.paster import (
+    get_appsettings,
+    setup_logging,
+    )
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from models import (
+    DBSession,
+    BdUsuario,
+    Base,
+    )
+
+
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri>\n'
+          '(example: "%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
+
+
+#def main(argv=sys.argv):
+from sys import argv
+if __name__ == '__main__':
+    if len(argv) != 2:
+        usage(argv)
+    config_uri = argv[1]
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri)
+    engine = engine_from_config(settings, 'sqlalchemy')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    with transaction.manager:
+        model = BdUsuario(nome='1',senha='11111')
+        DBSession.add(model)
+
--- wsgi/pyramidapp/setup.py
+++ wsgi/pyramidapp/setup.py
@@ -13,6 +13,9 @@ requires = [
     'sqlalchemy',
     'zope.sqlalchemy',
     'WebError',
+    'transaction',
+    'plim',
+    'deform',
     ]
 
 if sys.version_info[:3] < (2,5,0):
