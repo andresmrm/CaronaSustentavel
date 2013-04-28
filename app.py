e0b6384467cdfd1a307eedd562cd2a6d100f2b5f
andresmrm <andresmrm@gmail.com>
2013-04-28 00:42:17 -0300

    arrumando identacao

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ PYCART_DIR = ''.join(['python-', '.'.join(map(str, sys.version_info[:2]))])
 
 try:
     zvirtenv = os.path.join(os.environ['OPENSHIFT_HOMEDIR'], PYCART_DIR,
-        'virtenv', 'bin', 'activate_this.py')
+                            'virtenv', 'bin', 'activate_this.py')
     execfile(zvirtenv, dict(__file__=zvirtenv))
 except IOError:
     pass
