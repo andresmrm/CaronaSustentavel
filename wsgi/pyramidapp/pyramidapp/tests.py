dda0215902e316fc9a03bf7d427dd8bb90809961
unknown <Andre.Barata@brsaopbaratan5.ey.net>
2013-06-27 11:46:22 -0300

    testes

--- wsgi/pyramidapp/pyramidapp/tests.py
+++ wsgi/pyramidapp/pyramidapp/tests.py
@@ -1,14 +1,14 @@
 from selenium import selenium
 import unittest, time, re
 
-class sel_1(unittest.TestCase):
+class login(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
-        self.selenium = selenium('ondemand.saucelabs.com',80,"""{"username": "andremontoiab","access-key": "d95e5825-a4d4-4a27-9b6e-3b899f5b09e1","os": "Windows 2003","browser": "firefox","browser-version": "7","name": "Testing Selenium 1 in Python at Sauce"}""",'http://carona-sustentavel.rhcloud.com/login')
+        self.selenium = selenium('ondemand.saucelabs.com',80,"""{"username": "andremontoiab","access-key": "d95e5825-a4d4-4a27-9b6e-3b899f5b09e1","os": "Windows 2003","browser": "firefox","browser-version": "7","name": "Carona Sustentável"}""",'http://carona-sustentavel.rhcloud.com/login')
         self.selenium.start()			
     
-    def test_sel_1(self):
-        sel = self.selenium
+    def login(self):
+		sel = self.selenium
         sel.open("/login")
         sel.wait_for_page_to_load("60000")
         sel.click("id=content")
@@ -16,6 +16,7 @@ class sel_1(unittest.TestCase):
         sel.type("id=deformField2", "11111")
         sel.click("css=#deformEntrar")
         sel.wait_for_page_to_load("60000")
+        sel.click("xpath=//div[@class='mainContent']//li[.='Altura: 1.0']")
     
     def tearDown(self):
         self.selenium.stop()
