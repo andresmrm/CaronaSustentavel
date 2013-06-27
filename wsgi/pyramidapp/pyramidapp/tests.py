01a58cc2047341739949554ab241abaeddf1e0af
unknown <Andre.Barata@brsaopbaratan5.sa.ey.net>
2013-06-27 02:20:51 -0300

    Update tests

--- wsgi/pyramidapp/pyramidapp/tests.py
+++ wsgi/pyramidapp/pyramidapp/tests.py
@@ -31,9 +31,7 @@ def is_alert_present(wd):
 
 class login(unittest.TestCase):
     def setUp(self):	
-		desired_capabilities = webdriver.DesiredCapabilities.FIREFOX        
-        desired_capabilities['platform'] = 'Windowns 2003'
-        desired_capabilities['name'] = 'Carona Sustentável'
+		desired_capabilities = webdriver.DesiredCapabilities.FIREFOX       
 
         self.wd = webdriver.Remote(
             desired_capabilities=desired_capabilities,
