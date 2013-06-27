ca3da9c96037560e8bb7eaf958544fb847cb196d
unknown <Andre.Barata@brsaopbaratan5.ey.net>
2013-06-27 09:58:36 -0300

    testes

--- wsgi/pyramidapp/pyramidapp/tests.py
+++ wsgi/pyramidapp/pyramidapp/tests.py
@@ -1,46 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from selenium import selenium
+import unittest, time, re
 
-#-----------------------------------------------------------------------------
-# Copyright 2013 Carona Sustentavel
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
-#-----------------------------------------------------------------------------
-
-import unittest
-from selenium import webdriver
-
-class Selenium2OnSauce(unittest.TestCase):
-
+class selenium_1(unittest.TestCase):
     def setUp(self):
-        desired_capabilities = webdriver.DesiredCapabilities.CHROME
-        desired_capabilities['version'] = ''
-        desired_capabilities['platform'] = 'Windows 2003'
-        desired_capabilities['name'] = 'Carona Sustentável'
-
-        self.driver = webdriver.Remote(		
-            desired_capabilities=desired_capabilities,
-            command_executor="http://andremontoiab:d95e5825-a4d4-4a27-9b6e-3b899f5b09e1@ondemand.saucelabs.com:80/wd/hub"
-        )		         
-        self.driver.implicitly_wait(30)
-
-    def test_sauce(self):
-        self.driver.get('carona-sustentavel.rhcloud.com')
-
+        self.verificationErrors = []
+        self.selenium = selenium("http", //andremontoiab, "*chrome", "http://carona-sustentavel.rhcloud.com")
+        self.selenium.start()
+    
+    def test_selenium_1(self):
+        sel = self.selenium
+        sel.open("/login")
+        sel.wait_for_page_to_load("60000")
+        sel.click("css=div.inner1")
+        sel.type("id=deformField1", "test")
+        sel.type("id=deformField2", "11111")
+        sel.click("css=#deformEntrar")
+        sel.wait_for_page_to_load("60000")
+    
     def tearDown(self):
-        print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
-        self.driver.quit()		
-		
-if __name__ == '__main__':
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
     unittest.main()
